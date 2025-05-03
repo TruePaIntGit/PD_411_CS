e55345081799da544cd403ed4e2488a4bf661118
TruePaInt <>
2025-05-03 16:54:50 +0500

    All types

--- Introduction/DataTypes/Program.cs
+++ Introduction/DataTypes/Program.cs
@@ -13,22 +13,26 @@ namespace DataTypes
         static void Main(string[] args)
         {
 
-#if DATA_TYPES
-            Console.WriteLine("DataTypes");
-            Console.WriteLine(typeof(sbyte));
-            Console.WriteLine(sizeof(byte));
-            Console.WriteLine($"Byte:  {byte.MinValue} ... {byte.MaxValue}");
-            Console.WriteLine($"SByte:{sbyte.MinValue}... {sbyte.MaxValue}"); 
-#endif
+//#if DATA_TYPES
+//            Console.WriteLine("DataTypes");
+//            Console.WriteLine(typeof(sbyte));
+//            Console.WriteLine(sizeof(byte));
+//            Console.WriteLine($"Byte:  {byte.MinValue} ... {byte.MaxValue}");
+//            Console.WriteLine($"SByte:{sbyte.MinValue}... {sbyte.MaxValue}"); 
+//#endif
 
             Console.WriteLine("Hello Constants");
-            Console.WriteLine('+'.GetType());
-            Console.WriteLine(5.GetType());
-            Console.WriteLine(true.GetType());
+            Console.WriteLine($"Имя: { '+'.GetType(),-15} Размер: {sizeof(Char),-3} От: {Char.MinValue} До: {Char.MaxValue}");
+            Console.WriteLine($"Имя: {5.GetType(),-15} Размер: {sizeof(int),-3} От: {int.MinValue} До: {int.MaxValue}");
+            Console.WriteLine($"Имя: {true.GetType(),-15} Размер: {sizeof(bool),-3} От: {false} До: {true}");
+            Console.WriteLine($"Имя: {5.4.GetType(),-15} Размер: {sizeof(double),-3} От: {double.MinValue} До: {double.MaxValue}");
+            Console.WriteLine($"Имя: {typeof(uint).FullName,-15} Размер: {sizeof(uint),-3} От: {uint.MinValue} До: {uint.MaxValue}");
+            
 
             int a = -2;
             uint b = (uint)a;
             //C-like notation (C-подобная форма записи)
+            Console.ReadLine();
         }
     }
 }
